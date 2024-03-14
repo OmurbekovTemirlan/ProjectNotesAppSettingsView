1beaa605d0b99f47b3c7596ff0753a87432ac2b3
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-03-14 14:29:53 +0600

    Initial Commit

---
+++ ProjectNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectNotesApp
+//
+//  Created by Apple on 14.3.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
